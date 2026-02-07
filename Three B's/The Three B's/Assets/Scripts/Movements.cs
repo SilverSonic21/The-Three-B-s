f5476c149d20f42f2e7a4b253eb07cedda65596b
SilverSonic21 <146499245+SilverSonic21@users.noreply.github.com>
2026-02-07 12:52:27 -0600

    Capsule

---
+++ Three B's/The Three B's/Assets/Scripts/Movements.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float speed = 100f;
+    public float JumpHeight;
+    public bool InAir = false;
+
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        InAir = false;
+        Debug.Log("InAir false");
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        InAir = true;
+        Debug.Log("InAir True");
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 NoMovement = new Vector2(0f, 0f);
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        if (moveHorizontal > 0)
+        {
+            {
+                rb2d.linearVelocity = new Vector2(speed, rb2d.linearVelocity.y);
+
+            }
+        }
+        if (moveHorizontal < 0)
+        {
+            rb2d.linearVelocity = new Vector2(-speed, rb2d.linearVelocity.y);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) || (Input.GetKeyDown(KeyCode.UpArrow)))
+        {
+            if (InAir == false)
+            {
+                rb2d.AddForce(new Vector2(0, JumpHeight), ForceMode2D.Impulse);
+            }
+        }
+    }
+
+}
+
