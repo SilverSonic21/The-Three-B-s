b238c30a3144e2eb4cbfaadb3225a964810f15d9
SilverSonic21 <146499245+SilverSonic21@users.noreply.github.com>
2026-03-02 17:28:06 -0600

    Starting Screen

---
+++ Three B's/The Three B's/Assets/Scripts/PlayButton.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+public class PlayButton : MonoBehaviour
+{
+ 
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+
+    
+}
